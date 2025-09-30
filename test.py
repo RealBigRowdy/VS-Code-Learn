3f9d95ae5bc6ddc3bf15e6175194c6f8ff561ea2
RealBigRowdy <alirezasabour97@gmail.com>
2025-09-30 19:00:45 +0330

    first edit

---
+++ test.py
@@ -0,0 +1,21 @@
+a=5
+b=4
+print(a*b)
+
+name="Alireza"
+sen=20
+location="Shiraz"
+print("My name is",name,"And my age is",sen,"I live in",location)
+
+sen=20
+
+if sen < 25:
+    vaziat="salem"
+    print("Hanooz Javooni")
+
+if sen > 25:
+    vaziat="Aali"
+    print("Dari Bozorg Mishi")
+
+print(vaziat)
+
