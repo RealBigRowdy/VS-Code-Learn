ffb28a1b91b8ab8f6f6befb881eaf78f873deea4
RealBigRowdy <alirezasabour97@gmail.com>
2025-10-02 16:46:47 +0330

    exercise edit

---
+++ test2.py
@@ -0,0 +1,16 @@
+print("این ادامه تمرین های من هست")
+
+sen=20
+sen_pedar= 50
+
+if sen>20:
+    print("hanooz javooni")
+    print("sene pedar",sen_pedar,"bood?")
+
+if sen==20:
+    print("bozorg shodi")
+    print("sene pedar az",sen_pedar,"rade")
+
+if sen<20:
+    print("bache saali")
+    print("pedar ham ke",sen_pedar,"nashode")
