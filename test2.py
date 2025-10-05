25e40675479e39ca007dc212556d1ed33e938dad
RealBigRowdy <alirezasabour97@gmail.com>
2025-10-05 11:50:06 +0330

    avval edit

--- test2.py
+++ test2.py
@@ -19,3 +19,22 @@ for i in range (1,101):
     #In mishe tavan2?
     print(i,i*i)
     print("----")
+
+for i in {2,3,4,5,6,7}:
+    print("8 /",i,"=",8/i)
+    print("------")
+    
+print("8 adad aval nist")
+print("--")
+
+def is_prime(n):
+    aval=True
+    #har adadi bezari to n
+    for i in range(2,n):
+        if (n%i==0):
+            aval=False
+    return aval
+
+for i in range(1,1001):
+    if is_prime(i):
+        print (i, end=", ")
